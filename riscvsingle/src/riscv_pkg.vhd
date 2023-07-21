9888a0af7ca138ee7dc337bfa03585635ea6da4a
locochoco <ivanrwpf@gmail.com>
2023-07-21 15:26:49 -0300

    fixed funcao de soma

--- riscvsingle/src/riscv_pkg.vhd
+++ riscvsingle/src/riscv_pkg.vhd
@@ -135,15 +135,15 @@ package body riscv_pkg is
     end loop;
     return result;
   end to_integer;
-  function "+" (constant a: STD_LOGIC_VECTOR; constant b: STD_LOGIC_VECTOR) return STD_LOGIC_VECTOR is
-    variable result: STD_LOGIC_VECTOR(a'LENGTH - 1 downto 0) := (others => '0');
+   function "+" (constant a: STD_LOGIC_VECTOR; constant b: STD_LOGIC_VECTOR) return STD_LOGIC_VECTOR is
+    alias xa: STD_LOGIC_VECTOR(a'LENGTH - 1 downto 0) is a;
+    alias xb: STD_LOGIC_VECTOR(b'LENGTH - 1 downto 0) is b;
+    variable result: STD_LOGIC_VECTOR(a'LENGTH - 1 downto 0);
     variable carry: STD_LOGIC := '0';
-    variable halfSum: STD_LOGIC;
   begin
-    for i in 0 to a'LENGTH - 1 loop
-      halfSum:= a(i) or b(i);
-      result(i) := halfSum xor carry;
-      carry:= (a(i) and b(i)) or (halfSum and carry);
+    for i in 0 to xa'LENGTH - 1 loop
+		result(i) := xa(i) XOR xb(i) XOR carry;
+		carry := (xa(i) AND xb(i)) OR (carry AND xa(i)) OR (carry AND xb(i));
     end loop;
     return result;
   end "+";
